bc1668e21c2e0ee899fca1d118e710f3fb78e75b
Anna Andrienko <milesha10@inbox.ru>
2022-07-18 20:38:03 +0700

    task19  has been corrected twice

--- task19/Program.cs
+++ task19/Program.cs
@@ -3,4 +3,29 @@
 //14212 -> нет
 //12821 -> да
 //23432 -> да
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите пятизначное число");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 9999 || N < 100000){
+
+int n = N;
+int reverse = 0;
+int r;
+int count = 1;
+while ( count <=5 )
+{ 
+  r = n % 10;
+  reverse = reverse*10+r;
+  n = n/10;
+  count++;
+} 
+if (N == reverse)
+{
+  Console.WriteLine("Это палиндром");
+}
+else
+{
+  Console.WriteLine("Это не палиндром");
+}
+
+}
